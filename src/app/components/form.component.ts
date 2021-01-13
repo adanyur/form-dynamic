9be03037d31a9b8fcdedf050941cc33fb50f309a
Yur Adan Valdez Carazas <yvaldez@clinicasantaisabel.com>
2021-01-13 16:42:01 -0500

    v3 form dynamic

--- src/app/components/form.component.ts
+++ src/app/components/form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms'
-import { Observable , of} from 'rxjs'
+import { interval, fromEvent,Observable, of } from 'rxjs'
 
 
 import { FormService } from '../core/services/form.service';
 import { HttpService } from '../core/services/http.service';
 import { FormBase } from '../core/helps/form-base'
+import { switchMap,debounceTime,delay,filter,map } from 'rxjs/operators';
 
 
 @Component({
@@ -15,32 +16,62 @@ import { FormBase } from '../core/helps/form-base'
 })
 export class FormComponent implements OnInit {
 
-  forms$:Observable<FormBase<any>[]>;
+  forms$:Observable<FormBase<string>[]>;
   value$:Observable<any>;
+  especialidad$:Observable<any>;
+  data$:Observable<any>;
+
   form:FormGroup;
-  
-  formulario:any
+  formulario:FormBase<string>[]
 
 
   constructor(private formService:FormService,private fb:FormBuilder,private httpService:HttpService
               ) { }
 
   ngOnInit(): void {
-    this.form = this.fb.group({})
+    this.form = this.fb.group({
+      fecha:[null]
+    })
+    // this.especialidad$ = this.httpService.getEspecialidad()  
+    // this.data$ = this.form.get('fecha').value.subscribe(console.log)
+                // pipe(
+                //   debounceTime(1000),
+                //   map(data => data.replace(/-/g,'')),
+                //   switchMap((data:string)=> this.httpService.getProgamacion(data))
+                // )
   }
+
+  
+// getEspecialidad(){
+//   return this.httpService.getEspecialidad()
+// }
+
+getPro(data:any){
+  const fecha=data.replace(/-/g,'')
+  this.data$ = this.httpService.getProgamacion(fecha)
+}
 
 
 onForm(data:string){  
   this.httpService.getFormulario(data).subscribe((data:FormBase<string>[])=>{  
     this.form = this.formService.formGroup(data)
-    this.formulario=data.sort((a,b)=> a.order - b.order)
+    this.formulario = data
   })
 }
 
 
 onSubmit(){
-//  this.value$ = this.httpService.getFormulario(this.form.value)
-  this.value$ = of(this.form.value)
+  //  this.value$ = this.httpService.postEspecialidad(this.form.value).pipe()  
+  //  this.especialidad$ = this.getEspecialidad().pipe(debounceTime(2000))
+  //  this.getEspecialidad().subscribe(console.log)
+  //  this.httpService.getEspecialidad().subscribe(console.log)
+
+    of([1,2,3,4]).pipe(debounceTime(1000)).subscribe(console.log)
+    interval(1000).pipe(debounceTime(1000)).subscribe(console.log)
+
+   
+  //this.value$ = this.httpService.postRol(this.form.value)
+  //this.value$ = of(this.form.value)
 }
 
 
--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { pluck } from 'rxjs/operators';
+import { map, pluck, debounceTime } from 'rxjs/operators';
+
+import { FormBase } from '../helps/form-base'
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +15,31 @@ export class HttpService {
   getFormulario(data:string){
     const URL=`http://192.168.10.144:8001/formulario/${data}`
     return this.http.get(URL).pipe(
-      pluck('formulariod')
+      pluck('formulariod'),
+      map((data:FormBase<string>[])=> data.sort((a,b)=> a.order - b.order))
     );
   }
 
+  getEspecialidad(){
+    const URL ='http://192.168.10.144:8080/api/auth/especialidades'
+    return this.http.get(URL)
+  }
+
   postEspecialidad(data:any){
-    const URL ='http://192.168.10.144/'
+    const URL ='http://192.168.10.144:8080/api/auth/especialidades'
     return this.http.post(URL,data)
   }
 
+  postRol(data:any){
+    const URL ='http://192.168.10.144:8080/api/auth/rols'
+    return this.http.post(URL,data)
+  }
+
+
+  getProgamacion(fecha:string){
+    return this.http.get(`http://192.168.10.144:8082/api/programacion/${fecha}`)
+  }
+  
 
 
 }
